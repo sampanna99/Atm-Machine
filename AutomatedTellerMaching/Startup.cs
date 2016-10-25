57be18fa7892f7c12f6970ac8144fad16528ddb8
sampanna pokhrel <sampanna99@gmail.com>
2016-10-24 22:33:54 -0500

    Add project files.

---
+++ AutomatedTellerMaching/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomatedTellerMaching.Startup))]
+namespace AutomatedTellerMaching
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
