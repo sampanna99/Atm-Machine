f5c18058e854b2f3cc0f2595b25d6e2860882ca4
sampanna pokhrel <sampanna99@gmail.com>
2016-10-25 23:35:23 -0500

    validating the checkingaccount model

---
+++ AutomatedTellerMaching/Models/CheckingAccount.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomatedTellerMaching.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Account #")]
+        public string AccountNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+
+        [DataType(DataType.Currency)]
+        public int Balance { get; set; }
+
+    }
+}
